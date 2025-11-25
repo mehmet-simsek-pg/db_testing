94a5ced3d8e6e64ec7ceddaad1987fc8da9deef4
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-11-25 18:16:58 +0100

    BaseTest updated

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,36 @@
 package tests;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class BaseTest {
+
+    protected Connection connection;
+
+    @BeforeClass
+    public void setupConnection() throws SQLException {
+        Properties properties = getProperties();
+
+        String url = properties.getProperty("db.url");
+        String username = properties.getProperty("db.username");
+        String password = properties.getProperty("db.password");
+
+        connection = DriverManager.getConnection(url, username, password);
+    }
+
+    @AfterClass
+    public void tearDownConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
 
     private Properties getProperties() {
 
@@ -17,7 +43,7 @@ public class BaseTest {
             if (inputStream == null) { // Dosyanin olup olmadigini kontrol ediyoruz
                 throw new RuntimeException("db.properties dosyasi bulunamadi");
             } else {
-                properties.load(inputStream); // dosya var ise icerigini okutuyoruz
+                properties.load(inputStream); // dosya var ise icerigini yüklüyoruz
             }
         } catch (IOException e) {
             throw new RuntimeException("Dosya yüklenirken hata olustu");
