ab0bcbb17e0b732570398ebf33ce4cec8b1b2a45
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-11-25 17:28:47 +0100

    BasicConnectionTest created

---
+++ src/test/java/tests/BasicConnectionTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import java.sql.*;
+
+public class BasicConnectionTest {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:postgresql://localhost:5432/schooldb";
+        // url -> jdbc + database + server + port + database name
+        String user = "postgres";
+        String password = "postgres";
+
+        // Database baglandik
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        String sql = "SELECT * FROM students"; // sorguyu yazdik
+        Statement statement = connection.createStatement(); // durum olusturduk
+        ResultSet resultSet = statement.executeQuery(sql); // sorguyu calistirdik
+
+        while (resultSet.next()) { // tabloda kac veri oldugunu bilmedigim icin
+            // while döngüsü kullaniyoruz
+            // Database deki tablodan gelen verileri ekrana yazdirdik
+            System.out.println(
+                    resultSet.getInt("student_id") + " - " +
+                            resultSet.getString("first_name") + " - " +
+                            resultSet.getString("last_name") + " - " +
+                            resultSet.getString("email") + " - " +
+                            resultSet.getDate("birth_date")
+            );
+        }
+
+    }
+}
